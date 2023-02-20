47a1bfdc623d65bb55c5358f0d986a5a842ff17d
Yatzil Gutierrez-Nova <gutierrezyatzil@gmail.com>
2023-02-20 11:19:52 -0800

    MADE CONTACT FORM

--- dental/settings.py
+++ dental/settings.py
@@ -118,11 +118,13 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER= 'univdentalclinic@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'iahkowbyalapuvjt'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
 
 django_heroku.settings(locals())
